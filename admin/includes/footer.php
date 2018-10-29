89044123bf17fc64baf32f77475415c4cb10ce24
AshrafulAfruz <38793029+AshrafulAfruz@users.noreply.github.com>
2018-10-30 02:31:11 +0600

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,8 @@
+  
+  
+  <footer class="py-5 bg-inverse" style="z-index: 3;position: fixed;width: 100%;bottom: 0;">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; Bloodforces 2018</p>
+        </div>
+        <!-- /.container -->
+    </footer>
